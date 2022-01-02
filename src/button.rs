377cef734272a558b89e25fe66ff710503ba8e5c
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-01-02 15:18:55 +0900

    new file:   src/button.rs

---
+++ src/button.rs
@@ -0,0 +1,79 @@
+use bevy::{app::AppExit, prelude::*};
+
+const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
+
+#[derive(Component, Debug, Default)]
+pub struct ButtonPlugin;
+
+impl Plugin for ButtonPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(setup).add_system(button_system);
+    }
+}
+
+#[allow(clippy::type_complexity)]
+fn button_system(
+    mut app_exit_events: EventWriter<AppExit>,
+    mut interaction_query: Query<
+        (&Interaction, &mut UiColor, &Children),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut text_query: Query<&mut Text>,
+) {
+    for (interaction, mut color, children) in interaction_query.iter_mut() {
+        let mut text = text_query.get_mut(children[0]).unwrap();
+        match *interaction {
+            Interaction::Clicked => {
+                text.sections[0].value = "Press".to_string();
+                *color = PRESSED_BUTTON.into();
+                app_exit_events.send(AppExit);
+            }
+            Interaction::Hovered => {
+                text.sections[0].value = "QUIT".to_string();
+                *color = HOVERED_BUTTON.into();
+            }
+            Interaction::None => {
+                text.sections[0].value = "Button".to_string();
+                *color = NORMAL_BUTTON.into();
+            }
+        }
+    }
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // ui camera
+    commands.spawn_bundle(UiCameraBundle::default());
+    commands
+        .spawn_bundle(ButtonBundle {
+            style: Style {
+                // position_type: PositionType::Absolute,
+                size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+                // center button
+                margin: Rect::all(Val::Auto),
+                // horizontally center child text
+                justify_content: JustifyContent::Center,
+                // vertically center child text
+                align_items: AlignItems::Center,
+                ..Default::default()
+            },
+            color: NORMAL_BUTTON.into(),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            parent.spawn_bundle(TextBundle {
+                text: Text::with_section(
+                    "Button",
+                    TextStyle {
+                        // font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                        font: asset_server.load("fonts/NotoSansJP-Regular.otf"),
+                        font_size: 40.0,
+                        color: Color::rgb(0.9, 0.9, 0.9),
+                    },
+                    Default::default(),
+                ),
+                ..Default::default()
+            });
+        });
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use {
     bevy::prelude::*,
-    rg_001::{greet::GreetPlugin, text::MyTextPlugin},
+    rg_001::{button::ButtonPlugin, greet::GreetPlugin, text::MyTextPlugin},
 };
 
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
     // Defaultplugins is CorePlugin, InputPlugin and WindowPlugin.
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(GreetPlugin)
         .add_plugin(MyTextPlugin)
+        .add_plugin(ButtonPlugin)
         .run();
 }
