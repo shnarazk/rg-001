6278ca6fffe7f570627348af09c277dbdd401680
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-01-02 11:18:11 +0900

    new file:   src/greet.rs

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,10 @@
-use bevy::prelude::*;
-
-pub struct HelloPlugin;
-
-fn hello_world() {
-    println!("Hello, world!");
-}
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_startup_system(add_people);
-            // .add_system(hello_world);
-    }
-}
-
-#[derive(Component, Default)]
-struct Person;
-
-#[derive(Component, Default)]
-struct Name(String);
-
-fn add_people(mut commands: Commands) {
-    commands.spawn().insert(Person).insert(Name("Me".to_string()));
-    commands.spawn().insert(Person).insert(Name("Alice".to_string()));
-}
-
-pub struct GreetPlugin;
-struct GreetTimer(Timer);
-
-impl Plugin for GreetPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_system(greet_people);
-    }
-}
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("Hello {}!", name.0);
-        }
-    }
-}
-
+use {bevy::prelude::*, rg_001::greet::GreetPlugin};
 
 fn main() {
     println!("Hello, world!");
+    // Defaultplugins is CorePlugin, InputPlugin and WindowPlugin.
     App::new()
         .add_plugins(DefaultPlugins)
-        // .add_plugin(CorePlugin::default()) and InputPlugin, WindowPlugin
-        .add_plugin(HelloPlugin)
         .add_plugin(GreetPlugin)
         .run();
 }
